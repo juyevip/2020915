dba9743caac00a6d4994a28e93df63495429653a
Administrator <Administrator@teacher>
2020-09-15 14:38:52 +0800

    four commit

--- demo01-2/src/main/java/com/chinasoft/demo/entity/User.java
+++ demo01-2/src/main/java/com/chinasoft/demo/entity/User.java
@@ -6,4 +6,18 @@ public class User {
 	private int sno;
 	private String upw;
 	
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public String getUpw() {
+		return upw;
+	}
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+	
+	
 }
