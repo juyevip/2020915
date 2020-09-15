8a2fff8e1ee466c458c0e231a756cde8e8740613
Administrator <Administrator@teacher>
2020-09-15 14:12:46 +0800

    first commit

---
+++ demo01-2/src/test/java/com/chinasoft/demo/Demo012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinasoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
